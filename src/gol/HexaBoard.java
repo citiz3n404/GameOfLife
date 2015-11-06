925deaf46aaf0509cc248b50f846b086b0a27ddf
Jessica Favin <jessica00228@hotmail.fr>
2015-11-06 16:07:00 +0100

    initNonToriqueNeighbors for Hexa

--- src/gol/HexaBoard.java
+++ src/gol/HexaBoard.java
@@ -29,7 +29,49 @@ public class HexaBoard extends Board {
 
     @Override
     void initNonToriqueNeighbors() {
-        //TODO
+        for (int i = 0; i < Param.NB_ROWS; i++) {
+            for (int j = 0; j < Param.NB_COLUMNS; j++) {
+                if(i%2==1){  
+                    if (i > 0) {
+                        board[i][j].setNeighbor(SquareGridNbh.NORTH_WEST, board[i - 1][j]);
+                    }
+                    if (i > 0 && j < Param.NB_COLUMNS - 1) {
+                        board[i][j].setNeighbor(SquareGridNbh.NORTH_EAST, board[i - 1][j + 1]);
+                    }
+                    if (j < Param.NB_COLUMNS - 1) {
+                        board[i][j].setNeighbor(SquareGridNbh.EAST, board[i][j + 1]);
+                    }
+                    if (i < Param.NB_ROWS - 1&&j < Param.NB_COLUMNS - 1) {
+                        board[i][j].setNeighbor(SquareGridNbh.SOUTH_EAST, board[i + 1][j + 1]);
+                    }
+                    if (i < Param.NB_ROWS - 1) {
+                        board[i][j].setNeighbor(SquareGridNbh.SOUTH_WEST, board[i + 1][j]);
+                    }
+                    if (j > 0) {
+                        board[i][j].setNeighbor(SquareGridNbh.WEST, board[i][j - 1]);
+                    }
+                } else if(i%2==0){ 
+                    if (i > 0 && j > 0) {
+                        board[i][j].setNeighbor(SquareGridNbh.NORTH_WEST, board[i - 1][j - 1]);
+                    }
+                    if (i > 0) {
+                        board[i][j].setNeighbor(SquareGridNbh.NORTH_EAST, board[i - 1][j]);
+                    }
+                    if (j < Param.NB_COLUMNS - 1) {
+                        board[i][j].setNeighbor(SquareGridNbh.EAST, board[i][j + 1]);
+                    }
+                    if (i < Param.NB_ROWS - 1) {
+                        board[i][j].setNeighbor(SquareGridNbh.SOUTH_EAST, board[i + 1][j]);
+                    }
+                    if (i < Param.NB_ROWS - 1 && j > 0) {
+                        board[i][j].setNeighbor(SquareGridNbh.SOUTH_WEST, board[i + 1][j - 1]);
+                    }
+                    if (j > 0) {
+                        board[i][j].setNeighbor(SquareGridNbh.WEST, board[i][j - 1]);
+                    }
+                }
+            } 
+        }
     }
 
 }
