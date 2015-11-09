0012c06dadec3abdad0613783c7294f8ff21dcae
NightWolf <anthony92@live.fr>
2015-11-09 22:58:03 +0100

    Application's icon

--- src/gol/GoL.java
+++ src/gol/GoL.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 /**
@@ -19,7 +20,8 @@ public class GoL extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("View.fxml"));
         
         Scene scene = new Scene(root);
-        
+        stage.setTitle("Conway's Game of Life");
+        stage.getIcons().add(new Image(GoL.class.getResourceAsStream("icon.png")));
         stage.setScene(scene);
         stage.show();
     }
